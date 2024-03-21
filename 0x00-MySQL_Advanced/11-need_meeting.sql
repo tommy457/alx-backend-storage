09071c5a9a4702a74a5813002814c11d412b28d7
tommy457 <boukhalfaml1011@gmail.com>
2024-03-21 17:16:31 +0000

    task 11

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,5 @@
+-- script that creates a view need_meeting
+
+CREATE VIEW need_meeting AS
+SELECT name FROM students
+WHERE (score < 80 AND (last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH)));
