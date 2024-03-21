76a4ebbd58e620cef759814dc9c8bb5755a12b19
tommy457 <boukhalfaml1011@gmail.com>
2024-03-21 21:39:05 +0000

    task 100

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,18 @@
+-- script that creates a stored procedure ComputeAverageWeightedScoreForUser
+
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN intput_user_id INT)
+BEGIN
+    DECLARE average_weighted_score FLOAT;
+
+    SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight) INTO average_score
+    FROM corrections
+    JOIN projects
+    ON corrections.project_id = projects.id
+    WHERE corrections.user_id = intput_user_id;
+
+    UPDATE users
+    SET average_score = average_weighted_score 
+    WHERE id = intput_user_id;
+END$$
+DELIMITER ;
