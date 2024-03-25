c01415c7626367d000a815897eb8f769a2db60e1
tommy457 <boukhalfaml1011@gmail.com>
2024-03-25 09:37:24 -0700

    bug fixed

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,4 +6,4 @@ Module for the schools_by_topic function.
 
 def schools_by_topic(mongo_collection, topic):
     """ returns the list of school having a specific topic. """
-    return mongo_collection.find({"topic": topic})
+    return mongo_collection.find({"topics": topic})
