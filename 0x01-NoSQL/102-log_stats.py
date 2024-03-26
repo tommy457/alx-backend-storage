6bfd08c6fed1382503a38e66d1902d4535ed6f55
tommy457 <boukhalfaml1011@gmail.com>
2024-03-26 08:51:21 -0700

    code should not be executed when imported

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -3,38 +3,38 @@
 Module for listing some stats about Nginx logs stored in MongoDB.
 """
 from pymongo import MongoClient
+if __name__ == "__main__":
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["logs"]
+    nginx_collection = db["nginx"]
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["logs"]
-nginx_collection = db["nginx"]
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    total_count = nginx_collection.count_documents({})
 
-total_count = nginx_collection.count_documents({})
+    print("{} logs\nMethods:".format(total_count))
 
-print("{} logs\nMethods:".format(total_count))
-
-for method in methods:
-    print("    method {}: {}".format(
-        method,
-        nginx_collection.count_documents({"method": method})
+    for method in methods:
+        print("    method {}: {}".format(
+            method,
+            nginx_collection.count_documents({"method": method})
+            )
+        )
+    print("{} status check".format(
+        nginx_collection.count_documents({
+            "path": "/status",
+            "method": "GET"
+            })
         )
     )
-print("{} status check".format(
-    nginx_collection.count_documents({
-        "path": "/status",
-        "method": "GET"
-        })
-    )
-)
-pipeline = [
-    {"$group": {
-        "_id": "$ip",
-        "count": {"$sum": 1},
-    }},
-    {"$sort": {"count": -1}},
-    {"$limit": 10}
-]
-print("IPs:")
-for ip in nginx_collection.aggregate(pipeline):
-    print("\t{}: {}".format(ip["_id"], ip["count"]))
+    pipeline = [
+        {"$group": {
+            "_id": "$ip",
+            "count": {"$sum": 1},
+        }},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    print("IPs:")
+    for ip in nginx_collection.aggregate(pipeline):
+        print("\t{}: {}".format(ip["_id"], ip["count"]))
