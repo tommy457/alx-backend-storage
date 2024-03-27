4ed8db8efc4349462ec52871cfbd045570790851
tommy457 <boukhalfaml1011@gmail.com>
2024-03-27 13:44:35 +0000

    Storing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module for a Caching mechanism using Redis.
+"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """Represents a caching mechanism using Redis."""
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @property
+    def redis(self):
+        """ Getter property for the Redis client object. """
+        return self._redis
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Stores the provided data in the Redis database. """
+
+        key: str = uuid.uuid4()
+
+        self.redis.set(key, data)
+        return key
