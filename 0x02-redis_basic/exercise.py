f15ef6f58d0f7178ed1ddf4aadd72b1fc22be1a7
tommy457 <boukhalfaml1011@gmail.com>
2024-03-27 15:06:27 +0000

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,9 +2,24 @@
 """
 Module for a Caching mechanism using Redis.
 """
+from functools import wraps
 import redis
-from typing import Union, Callable
+from typing import Union, Callable, Any
 import uuid
+
+
+def count_calls(method: Callable) -> Callable:
+    """ decorator that takes a single method """
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Any:
+        """ wrapper function for counting how
+        many times a function is callded """
+        self.redis.incr(method.__qualname__)
+
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -18,6 +33,7 @@ class Cache:
         """ Getter property for the Redis client object. """
         return self._redis
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Stores the provided data in the Redis database. """
         key: str = str(uuid.uuid4())
@@ -40,3 +56,13 @@ class Cache:
     def get_str(self, key: str) -> str:
         """ Converts the returned redis byte string to an str. """
         return str(self.redis.get(key))
+
+
+cache = Cache()
+
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
+
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
