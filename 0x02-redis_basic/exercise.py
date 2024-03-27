18cf45d12ff48da448c786dd58f475844fc53841
tommy457 <boukhalfaml1011@gmail.com>
2024-03-27 14:17:30 +0000

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Module for a Caching mechanism using Redis.
 """
 import redis
-from typing import Union
+from typing import Union, Callable
 import uuid
 
 
@@ -20,8 +20,22 @@ class Cache:
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Stores the provided data in the Redis database. """
-
         key: str = str(uuid.uuid4())
 
         self.redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable) -> Union[str, int, float, bytes]:
+        """ Converts the returned redis byte string to the
+        desired format with the callable `fn`"""
+        if fn:
+            return fn(self.redis.get(key))
+        return self.redis.get(key)
+
+    def get_int(self, key: str) -> int:
+        """ Converts the returned redis byte string to an int. """
+        return int(self.redis.get(key))
+
+    def get_str(self, key: str) -> str:
+        """ Converts the returned redis byte string to an str. """
+        return str(self.redis.get(key))
